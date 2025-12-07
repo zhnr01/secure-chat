78c07092b8e3afb6df2ba7b9f241aa29908a9e98
zhnr01 <zeeshanrabnawaz.se@gmail.com>
2025-12-07 09:49:25 +0500

    Add simple logging utility with stream handler and formatter

---
+++ logging_util.py
@@ -0,0 +1,12 @@
+import logging
+
+
+def setup_logger(name: str = "secure_chat", level: int = logging.INFO) -> logging.Logger:
+    logger = logging.getLogger(name)
+    if not logger.handlers:
+        logger.setLevel(level)
+        handler = logging.StreamHandler()
+        formatter = logging.Formatter('%(asctime)s %(levelname)s %(name)s: %(message)s')
+        handler.setFormatter(formatter)
+        logger.addHandler(handler)
+    return logger
